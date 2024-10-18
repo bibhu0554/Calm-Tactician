98c62c2c54b25518741dff15b0dcdd63b9afbd19
Siffon <116371108+Steve1063498@users.noreply.github.com>
2024-10-18 12:11:20 +0000

    Hello

---
+++ Book_a_mentor.js
@@ -0,0 +1,20 @@
+// let buttonDiv = document.getElementById("name");
+
+// let targetDiv = document.getElementById("target-div");
+
+
+
+// buttonDiv.addEventListener("click", function() {
+
+//   if (targetDiv.style.display === "none") {
+
+//     targetDiv.style.display = "block";
+
+//   } else {
+
+//     targetDiv.style.display = "none";
+
+//   }
+
+// });
+
